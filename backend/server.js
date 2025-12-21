a50520cb1dc197aa1237efd7923e433924397cb2
Utkarsh Singh <utkarshsingh2513@gmail.com>
2025-12-21 18:02:46 +0530

    Fixed bug

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://geta-doc.vercel.app", "http://localhost:5173"],
+    origin: "http://localhost:5173",
     credentials: true,               // allow cookies
   })
 );
--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  // baseURL: "http://localhost:8000/api",
-  baseURL: "https://getadoc.onrender.com/api",
+  baseURL: "http://localhost:8000/api",
   withCredentials: true, // 🔑 REQUIRED for JWT cookies
   headers: {
     "Content-Type": "application/json",
